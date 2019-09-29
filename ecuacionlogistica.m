077bf553c9c1dc602c7907ad93fa85d85ca33c40
Félix Lagonell <47460629+feliux@users.noreply.github.com>
2019-09-29 19:29:50 +0200

    Update ecuacionlogistica.m

--- ecuacionlogistica.m
+++ ecuacionlogistica.m
@@ -1,20 +1,24 @@
 function xn=ecuacionlogistica(niter)
 %
+% xn=ecuacionlogistica(niter)
+%
 % Representación de la ecuación logística poblacional x(n+1)=C*x(n)*(1-x(n))
-% Uso: xn=logistica(niter)
 % Las diferentes órbitas se producen al cambiar el parámetro C.
+%
 % Este programa itera la ecuación logística para 600 valores diferentes de C
 % commprendidos entre 1 y 4.
 % Para cada valor de C se calcula una órbita de 600 valores, de los cuales
 % no se representan los 100 primeros ya que la función todavía no se ha
 % estabilizado.
-% 'niter' -> número iteraciones para establizar la ecuación. Tiene que ser
-% mayor que 100.
 %
-c=1:0.005:4; %Evaluamos todas las ctes ecológicas 'c'.
+% Variable de entrada:
+% niter = número iteraciones para establizar la ecuación. 
+% Tiene que ser mayor que 100.
+%
+c=1:0.005:4;                 % Evaluamos todas las ctes ecológicas 'c'.
 x=linspace(0.1,1,length(c)); % Creamos vector para el porcentaje de población óptima 'x'.
-h=niter-100; % Para representar a partir de la iteración 100.
-for i=1:100  % Para las primeras 100 iteraciones.
+h=niter-100;                 % Para representar a partir de la iteración 100.
+for i=1:100                  % Para las primeras 100 iteraciones.
     yn=c.*x.*(1-x);
     x=yn;
 end
