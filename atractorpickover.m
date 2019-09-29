f0cd65207a9026400d34e9aabfb971bd3886d3b1
Félix Lagonell <47460629+feliux@users.noreply.github.com>
2019-09-29 14:54:31 +0200

    Update atractorpickover.m

--- atractorpickover.m
+++ atractorpickover.m
@@ -1,17 +1,17 @@
 function atractorpickover(x1,y1,h,iter)
 %
-%Dibuja el atractor de Pickover.
-%Variar parámetros x1 y1 h para ver distintas formas.
-%atractorpickover(x1,y1,h,iter)
-%Recomendable un gran número de iteracioines == 1000 ó 10000.
-%ej:h=-0.02
+% Dibuja el atractor de Pickover.
+% Variar parÃ¡metros x1, y1, h para ver distintas formas.
+% atractorpickover(x1,y1,h,iter)
+% Recomendable un gran nÃºmero de iteracioines == 1000 Ã³ 10000.
+% EJ: h=-0.02
 %
 for i=1:iter
-    xn=x1-h.*sin(y1+tan(3.*y1));  %Ecuaciones
-    yn=y1-h.*sin(x1+tan(3.*x1));
+    xn=x1-h.*sin(y1+tan(3.*y1));  % Ecuaciones
+    yn=y1-h.*sin(x1+tan(3.*x1));  % Ecuaciones
     x1=xn;
     y1=yn;
-    plot(xn,yn,'k','markersize',2) %Markersize 2 para el tamaño de los ptos.
+    plot(xn,yn,'k','markersize',2)
     pause(0.0001)
     hold on
 end
